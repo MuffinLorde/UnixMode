aaf4b2e7d2cc6913b0be68316fde3d7a4e0b8e88
SoapyTheToast <scromskrom@gmail.com>
2025-01-03 11:02:04 +0200

    Fixed more slashes Finally added UnixMode version command

--- src/help.c
+++ src/help.c
@@ -6,19 +6,6 @@
 #include <ctype.h>
 #include "help.h"
 
-void listCommand(char command[32], char description[256]) {
-    char upperCommand[32];
-    int i;
-
-    for (i = 0; command[i] != '\0' && i < 31; i++) {
-        upperCommand[i] = toupper(command[i]);
-    }
-    upperCommand[i] = '\0';
-
-    printf("%s      %s\n", upperCommand, description);
-    return 0;
-}
-
 void helpList() {
     printf("For more information type help [command-name]\n\n");
     printf("CLEAR       clears the cmd\n");
@@ -27,13 +14,4 @@ void helpList() {
     printf("ECHO        Prints a message onto the screen\n");
     printf("EXIT        Exits UnixMode\n");
     printf("VERSION     States which version of UnixMode you have\n\n");
-
-/*    printf("For more information type help [command-name]\n\n");
-    listCommand("clear      ", "Clears the cmd");
-    listCommand("echo       ", "Prints a message onto the screen");
-    listCommand("pwd        ", "Prints the working directory");
-    listCommand("sdp        ", "Sets the default directory UnixMode enters into");
-    listCommand("exit       ", "Exits UnixMode");
-    listCommand("version    ", "States which version of UnixMode you have");
-    printf("\n"); */
 }
